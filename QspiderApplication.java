8a9a4b2e1ef5cb9e00ae96e597ca1f3d16f5e5d8
maheshpatil3031 <maheshpatil9168@gmail.com>
2023-02-25 13:00:28 +0530

    delete

--- QspiderApplication.java
+++ QspiderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@delete Service
 public class QspiderApplication {
 
 	public static void main(String[] args) {
